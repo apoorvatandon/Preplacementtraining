0301d1daece4c1fb15dc2118ac8b4486255ac9fe
apoorvatandon <55032664+apoorvatandon@users.noreply.github.com>
2022-07-24 20:01:03 +0530

    Create diametertree.java

---
+++ diametertree.java
@@ -0,0 +1,42 @@
+class Node {
+    int data;
+    Node left, right;
+ 
+    public Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    int diameter(Node root)
+    {
+
+        if (root == null)
+            return 0;
+
+        int lheight = height(root.left);
+        int rheight = height(root.right);
+ 
+        int ldiameter = diameter(root.left);
+        int rdiameter = diameter(root.right);
+
+        return Math.max(lheight + rheight + 1,
+                        Math.max(ldiameter, rdiameter));
+    }
+
+    int diameter() { return diameter(root); }
+
+    static int height(Node node)
+    {
+
+        if (node == null)
+            return 0;
+
+        return (1
+                + Math.max(height(node.left),
+                           height(node.right)));
+    }
